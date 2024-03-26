2d5011717e51c1e37645601dc902b0d25ddca0c7
JA-Dilao <69741368+Cuject@users.noreply.github.com>
2024-03-26 11:59:49 +0800

    Chart Component
    Am able to change the value of the chart as a separate component using an outside reference
    
    relied on the bind:this feature

--- src/lib/components/Chart.svelte
+++ src/lib/components/Chart.svelte
@@ -1,54 +1,59 @@
 <script>
-    import { Pie } from 'svelte-chartjs';
-    export let water  ;
-    export let protein;
-    export let fat    ;
-    export let carb   ;
-    export let ash    ;
-    export let fiber  ;
-    export let sugar  ;
+  import { Pie } from 'svelte-chartjs';
+  /*export let water   = 1;
+  export let protein = 2;
+  export let fat     = 3;
+  export let carb    = 4;
+  export let ash     = 5;
+  export let fiber   = 6;
+  export let sugar   = 7;*/
+
+  let listData = [1, 2, 3, 4];
 
 
-    const data = {
-        labels: ['Water', 'Protein', 'Total Fat', 'Carbohydrates', 'Ash', 'Fiber', 'Sugars'],
-        datasets: [
-            {
-            data: [water, protein, fat, carb, ash, fiber, sugar],
-            backgroundColor: [
-                '#46BFBD',
-                '#F7464A',
-                '#FDB45C',
-                '#949FB1',
-                '#4D5360',
-                '#AC64AD',
-            ],
-            hoverBackgroundColor: [
-                '#FF5A5E',
-                '#5AD3D1',
-                '#FFC870',
-                '#A8B3C5',
-                '#616774',
-                '#DA92DB',
-            ],
-            
-            },
-        ],
-    };
-  
-    import {
-      Chart as ChartJS,
-      Title,
-      Tooltip,
-      Legend,
-      ArcElement,
-      CategoryScale,
-    } from 'chart.js';
-  
-    ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+  const data = {
+      labels: ['Water', 'Protein', 'Total Fat', 'Carbohydrates', 'Ash', 'Fiber', 'Sugars'],
+      datasets: [
+          {
+          data: listData,
+          backgroundColor: [
+              '#46BFBD',
+              '#F7464A',
+              '#FDB45C',
+              '#949FB1',
+              '#4D5360',
+              '#AC64AD',
+          ],
+          hoverBackgroundColor: [
+              '#FF5A5E',
+              '#5AD3D1',
+              '#FFC870',
+              '#A8B3C5',
+              '#616774',
+              '#DA92DB',
+          ],
+          
+          },
+      ],
+  };
 
-    
-  </script>
+  export function changeData (list_values) {
+      data.datasets[0].data = list_values;
+  }
+
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    CategoryScale,
+  } from 'chart.js';
+  ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
   
+</script>
+
 <div>
   <Pie {data} options={{ responsive: true,
       plugins: {
@@ -62,9 +67,3 @@
   } }} />
 </div>
 
-
-<style>
-  div{
-    height: 200px;
-  }
-</style>
